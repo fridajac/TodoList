e7531be5788489d326c90d4bda5f3474cd79566a
fridajac <frida_jacobsson@live.se>
2022-04-05 15:11:51 +0200

    Created functionality for input for new task

--- src/comps/TextInputField.js
+++ src/comps/TextInputField.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
 const TextInputField = () => {
+  const [text, setText] = useState("");
+
+  const submitValue = () => {
+    const inputText = text;
+    alert(inputText);
+  };
+
   const inputStyle = {
     width: "480px",
     padding: "10px",
     fontFamily: "Noto Serif",
     fontSize: "1.5rem",
+    textTransform: "uppercase",
   };
-  const handleSubmit = (e) => {
-    alert("Submitted: " + e.target.value);
-  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" style={inputStyle}></input>
+    <form onSubmit={submitValue}>
+      <input
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        style={inputStyle}
+      ></input>
     </form>
   );
 };
