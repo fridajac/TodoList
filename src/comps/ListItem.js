bf6b9d21d8749b9c94ae566ecc4aec9dfd53e19b
fridajac <frida_jacobsson@live.se>
2022-04-06 11:17:22 +0200

    Changed ListItem to a label and checkbox

--- src/comps/ListItem.js
+++ src/comps/ListItem.js
@@ -3,32 +3,29 @@ import React from "react";
 const ListItem = (props) => {
   const [inHover, setHover] = useState(false);
   const [checked, setChecked] = useState(false);
-  const liStyle = {
-    padding: "5px",
+  const labelStyle = {
+    padding: "20px",
     marginTop: "10px",
     width: "500px",
     listStyleType: "none",
     textTransform: "uppercase",
     fontSize: inHover ? "25px" : "20px",
-    textDecoration: checked ? "line-through" : "none",
+    //textDecoration: checked ? "line-through" : "none",
+    checked,
   };
+
   return (
-    <div
-      className="listItem"
-      style={liStyle}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      onClick={() => (checked ? setChecked(false) : setChecked(true))}
-    >
-      <li>
-        <input
-          value={props.value}
-          className="checkbox"
-          type="checkbox"
-          checked={checked}
-        />
+    <div>
+      <label
+        style={labelStyle}
+        className="listLabel"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onClick={() => (checked ? setChecked(false) : setChecked(true))}
+      >
+        <input type="checkbox" />
         {props.value}
-      </li>
+      </label>
     </div>
   );
 };
