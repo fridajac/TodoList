cf0d92abdf5296ebe0a31cf16b882d6e2564197b
fridajac <frida_jacobsson@live.se>
2022-04-06 14:41:21 +0200

    Added button for deleting items

--- src/comps/List.js
+++ src/comps/List.js
@@ -1,15 +1,17 @@
 import ListItem from "./ListItem";
 const List = ({ todos }) => {
-  const listItems = todos.map((element, index) => (
-    <ListItem key={index} value={element} />
-  ));
+  const deleteItem = (e) => {
+    e.preventDefault();
+    console.log("hallååå");
+  };
+
   return (
     <div className="list">
       {
         <ul style={{ listStyleType: "none", marginTop: "3rem" }}>
           {todos.map((element, index) => (
-            <li key={index}>
-              <ListItem value={element} />
+            <li key={index} style={{ marginTop: "1.5rem" }}>
+              <ListItem value={element} deleteItem={deleteItem} />
             </li>
           ))}
         </ul>
--- src/comps/ListItem.js
+++ src/comps/ListItem.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import React from "react";
-const ListItem = (props) => {
+import Icon from "./trash-bin.png";
+
+const ListItem = (props, { deleteItem }) => {
   const [inHover, setHover] = useState(false);
   const [checked, setChecked] = useState(false);
   const labelStyle = {
     fontSize: inHover ? "25px" : "20px",
     textDecoration: checked ? "line-through" : "none",
+    color: checked ? "grey" : "black",
+    cursor: "pointer",
   };
 
   return (
@@ -21,6 +25,22 @@ const ListItem = (props) => {
           onClick={() => (checked ? setChecked(false) : setChecked(true))}
         ></input>
         {props.value.text}
+        <button
+          className="btn"
+          style={{
+            backgroundColor: "white",
+            color: "red",
+            marginLeft: "30px",
+            border: "none",
+          }}
+        >
+          <img
+            src={Icon}
+            alt="Delete"
+            style={{ width: { inHover } ? "30px" : "10px" }}
+            onClick={(e) => deleteItem(e.target.value)}
+          ></img>
+        </button>
       </label>
     </div>
   );
